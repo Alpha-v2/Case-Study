0457afcabaf1f65d7758c010834f28dd98f33eaf
root <root@ip-172-31-83-13.ec2.internal>
2023-12-23 07:46:52 +0000

    develop commit

--- First.tf
+++ First.tf
@@ -1,5 +1,5 @@
 provider "aws" {
    access_key = "AKIA6DCU3KTYY6F7UJYT"
-   secret_key = "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"
+ //  secret_key = "JttoA8px7gTUPBD7nIKNXa9KMMDb73Pef0N39k0G"
    region  = "us-east-1"
 }
